b36f2312ed9d6802c184483829cb035a8df94477
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2023-04-24 15:50:03 +0530

    Button removed

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,62 +1,62 @@
 // import React from 'react'
-import React, { useEffect } from "react";
-import { jsPDF } from "jspdf"; // jspdf is library to create pdf
+import React, { useEffect } from 'react'
+// import { jsPDF } from "jspdf";  jspdf is library to create pdf
 
-import { fabric } from "fabric";
+import { fabric } from 'fabric'
 
 const Canvas = () => {
   // using hooks as compnentdidupdate, only once,
   useEffect(() => {
-    const canvas = new fabric.Canvas("my-fabric-canvas", {
+    const canvas = new fabric.Canvas('my-fabric-canvas', {
       isDrawingMode: true,
-      color: "red",
-    });
+      color: 'red',
+    })
 
     //createing circle
     const circle = new fabric.Circle({
       radius: 5,
-      fill: "blue",
+      fill: 'blue',
       top: 10,
       left: 1 + 390,
-    });
+    })
     // creating text box
-    const textbox = new fabric.Textbox("You can start to draw", {
+    const textbox = new fabric.Textbox('You can start to draw', {
       fontSize: 20,
       left: 15 + 400,
       top: 5,
       width: 200,
-    });
+    })
 
     //adding shapes to canvas
-    canvas.add(textbox);
-    canvas.add(circle);
-  }, []);
+    canvas.add(textbox)
+    canvas.add(circle)
+  }, [])
 
   //handling screen-shot
-  const handleScreenshot = () => {
-    const doc = new jsPDF();
-    doc.text("I Should work on canvas image to pdf converter", 10, 10);
-    doc.save("a4.pdf");
-    console.log("screen");
-  };
+  // const handleScreenshot = () => {
+  //   const doc = new jsPDF();
+  //   doc.text("I Should work on canvas image to pdf converter", 10, 10);
+  //   doc.save("a4.pdf");
+  //   console.log("screen");
+  // };
 
   return (
-    <div className="App">
+    <div className='App'>
       <center>
         <canvas
-          id="my-fabric-canvas"
-          className="color"
-          width="1000"
-          height="550"
+          id='my-fabric-canvas'
+          className='color'
+          width='1000'
+          height='550'
         />
-        <button
+        {/* <button
           className="btn btn-primary mt-2"
           onClick={() => handleScreenshot()}>
           Take a screenshot and download
-        </button>
+        </button> */}
       </center>
     </div>
-  );
-};
+  )
+}
 
-export default Canvas;
+export default Canvas
